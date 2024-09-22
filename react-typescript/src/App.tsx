c3ce9faf3f522f766f8cbe4164ea32f06ecc5899
Sina <sinaderik7@gmail.com>
2024-09-22 14:00:14 +0330

    feat: List component added to the project

--- react-typescript/src/App.tsx
+++ react-typescript/src/App.tsx
@@ -1,17 +1,25 @@
-import { useState } from "react"
+import {useState } from "react"
 import Counter from "./components/Counter"
 import Heading from "./components/Heading"
 import { Section } from "./components/Section"
 import './index.css'
+import List from "./components/List"
 
 function App() {
   const [count, setCount] = useState<number>(0)
-
+  
+  function renderItem(item: string): JSX.Element {
+    return (
+      <span className="gold">{item}</span>
+    )
+  }
+  
   return (
     <>
       <Heading title="This is header" />
       <Section>This is the section component</Section>
       <Counter setCount={setCount}>The count is: {count}</Counter>
+      <List items={['coffee','tea']} render={renderItem}/>
     </>
   )
 }
