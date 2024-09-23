67563d8d3a93f3d652267f58e757d2f23e358c2d
Sina <sinaderik7@gmail.com>
2024-09-23 12:50:44 +0330

    feat useMemo is added to keep the calculation result of fibonacci function

--- react-hooks/src/App.tsx
+++ react-hooks/src/App.tsx
@@ -1,8 +1,10 @@
-import { KeyboardEvent, MouseEvent, useCallback, useEffect, useState } from "react"
+import { KeyboardEvent, MouseEvent, useCallback, useEffect, useMemo, useState } from "react"
 interface Users {
   id: number,
   username: string
 }
+type Fibonacci = (n: number) => number
+
 const App = () => {
   const [count, setCount] = useState<number>(0)
   const [users, setUsers] = useState<Users[] | null>(null)
@@ -14,14 +16,23 @@ const App = () => {
     return () => console.log('unmount')
   }, [users])
 
+  const fibonacci: Fibonacci = (n) => {
+    if (n < 2) return n
+    return fibonacci(n - 1) + fibonacci(n - 2)
+  }
+
   const countPlusTwo = useCallback((e: MouseEvent<HTMLButtonElement> | KeyboardEvent<HTMLButtonElement>): void => {
     setCount(previous => previous + 2)
   }, [])
 
+  const fibonacciNumber = 39
+  const result: number = useMemo<number>(() => fibonacci(fibonacciNumber), [fibonacciNumber])
+  
   return (
     <>
       <h1>{count}</h1>
       <button onClick={countPlusTwo}>+ 2</button>
+      <h2>Fibonacci result: {result}</h2>
     </>
   )
 }
