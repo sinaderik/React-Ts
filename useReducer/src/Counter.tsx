4a0f941b8ebbd161a2667e2c327babb37e9a61a4
Sina <sinaderik7@gmail.com>
2024-09-23 14:53:07 +0330

    feat: useReducer project is initilized

---
+++ useReducer/src/Counter.tsx
@@ -0,0 +1,20 @@
+import { ReactNode, useState } from "react"
+
+type Children = {
+    children: (n: number) => ReactNode
+}
+
+const Counter = ({ children }: Children) => {
+    const [count, setCount] = useState<number>(0)
+    return (
+        <>
+            <h1>Count: {children(count)}</h1>
+            <div>
+                <button onClick={() => setCount(prev => prev + 1)}>+</button>
+                <button onClick={() => setCount(prev => prev - 1)}>-</button>
+            </div>
+        </>
+    )
+}
+
+export default Counter
