521f23704e54fbb9a28cc16b0baaddea46b546bb
Sina <sinaderik7@gmail.com>
2024-09-30 23:24:37 +0330

    feat: operation to add the quantity is implemented

--- cart-typescript/src/context/CartProvider.tsx
+++ cart-typescript/src/context/CartProvider.tsx
@@ -1,3 +1,10 @@
+const REDUCER_ACTION_TYPES = {
+    ADD: 'ADD',
+    REMOVE: 'REMOVE',
+    SUBMIT: 'SUBMIT',
+    QUANTITY: 'QUANTITY',
+}
+
 export type CartItemType = {
     sku: string,
     name: string,
@@ -7,10 +14,39 @@ export type CartItemType = {
 
 export type CartStateType = { cart: CartItemType[] }
 
-const REDUCER_ACTION_TYPES={
-    ADD:'ADD',
-    REMOVE:'REMOVE',
-    SUBMIT:'SUBMIT',
-    QUANTITY:'QUANTITY',
-}
+export type ReducerActionType = typeof REDUCER_ACTION_TYPES
+export type ReducerAction = { type: string, payload: CartItemType }
 
+const initialCartState: CartStateType = { cart: [] }
+
+const reducer = (state: CartStateType, action: ReducerAction): CartStateType => {
+    switch (action.type) {
+        case REDUCER_ACTION_TYPES.ADD: {
+            if (!action.payload) throw new Error('action.payload is not provided')
+            const { name, price, sku } = action.payload
+
+            // return all the items that we dont want to update
+            const filteredItems: CartItemType[] = state.cart.filter(item => item.sku !== sku)
+
+            // return the only items that we want to update
+            const existingItem: CartItemType | undefined = state.cart.find(item => item.sku === sku)
+            const qty: number = existingItem ? existingItem.qty + 1 : 1
+
+            return {
+                ...state, cart: [...filteredItems, { sku, name, price, qty }]
+            }
+        }
+        case REDUCER_ACTION_TYPES.QUANTITY: {
+
+        }
+        case REDUCER_ACTION_TYPES.REMOVE: {
+
+        }
+        case REDUCER_ACTION_TYPES.SUBMIT: {
+            return { ...state, cart: [] }
+        }
+        default: {
+            return state
+        }
+    }
+}
